6fb064ecaf04a294fe0fe5fa70ea46a9279508ba
Amilcar Rey <amilcarrey.ar@gmail.com>
2020-09-06 23:28:05 -0300

    Adding .env support

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,6 +47,7 @@ export default {
   buildModules: [
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/dotenv',
   ],
   /*
   ** Nuxt.js modules
