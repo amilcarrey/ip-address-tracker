19ba20a399d46a950b63a0d75d93a2b3af8a5348
Amilcar Rey <amilcarrey.ar@gmail.com>
2020-09-11 16:28:35 -0300

    Base component created

---
+++ components/Map.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div id="map-wrap" style="height: 100vh">
+      <no-ssr>
+        <l-map :zoom="13" :center="lat_lng">
+          <l-tile-layer
+            url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+          ></l-tile-layer>
+          <l-marker :lat-lng="lat_lng"></l-marker>
+        </l-map>
+      </no-ssr>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        lat_lng: {
+            type: Array,
+            default: [-34.5349754, -58.5224896]
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
