2d1180599c4268a119b3cb24fa2164954d57390a
Amilcar Rey <amilcarrey.ar@gmail.com>
2020-09-11 18:24:07 -0300

    Extracting Title to a component

---
+++ components/Title.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1 class="font-semibold 
+  font-sans 
+  text-5xl 
+  text-white 
+  text-center 
+  mb-6">
+    IP Address Tracker
+  </h1>
+</template>
+
+<script>
+export default {
+    name: "Title"
+};
+</script>
+
+<style lang="scss" scoped></style>
